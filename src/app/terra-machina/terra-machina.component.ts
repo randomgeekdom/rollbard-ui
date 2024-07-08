9a7f6993f99df87da13c8be19738e10e6593e407
Anthony C. Jones <randomgeekdom@gmail.com>
2024-07-07 21:10:40 -0500

    adding terra machina download page

--- src/app/terra-machina/terra-machina.component.ts
+++ src/app/terra-machina/terra-machina.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-terra-machina',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './terra-machina.component.html',
   styleUrl: './terra-machina.component.scss'
 })
