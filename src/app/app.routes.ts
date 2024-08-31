d13eee7d7883d4814b163daca8959271dc0520f5
Anthony C. Jones <randomgeekdom@gmail.com>
2024-08-30 21:06:09 -0500

    Starting zumaroeth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TerraMachinaComponent } from './terra-machina/terra-machina.component';
 import { CharacterGeneratorComponent } from './character-generator/character-generator.component';
+import { ZumaroethComponent } from './zumaroeth/zumaroeth.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'terra-machina', component: TerraMachinaComponent },
-    { path: 'character-generator', component: CharacterGeneratorComponent }
+    { path: 'character-generator', component: CharacterGeneratorComponent },
+    { path: 'zumaroeth', component: ZumaroethComponent }
 
 ];
---
+++ src/app/zumaroeth/zumaroeth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zumaroeth',
+  standalone: true,
+  imports: [],
+  templateUrl: './zumaroeth.component.html',
+  styleUrl: './zumaroeth.component.scss'
+})
+export class ZumaroethComponent {
+
+}
