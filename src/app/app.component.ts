7aa334c3c7482a198a98379f80f306c0c3e83644
Anthony C. Jones <randomgeekdom@gmail.com>
2024-11-22 19:07:38 -0600

    Cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 
 @Component({
--- src/app/character-roller/character-roller.component.ts
+++ src/app/character-roller/character-roller.component.ts
@@ -2,7 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { Gender, NPC, NPCGenerator } from '@randomgeekdom/rollbard';
@@ -14,8 +16,10 @@ import { Gender, NPC, NPCGenerator } from '@randomgeekdom/rollbard';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
+    MatGridListModule,
     ReactiveFormsModule,
     CommonModule,
+    MatCardModule,
     FormsModule],
   templateUrl: './character-roller.component.html',
   styleUrl: './character-roller.component.scss'
@@ -23,6 +27,7 @@ import { Gender, NPC, NPCGenerator } from '@randomgeekdom/rollbard';
 export class CharacterRollerComponent {
   npcGenerator = new NPCGenerator();
   npc!: NPC;
+  characters: NPC[] = [];
 
   constructor() {
     this.roll();
@@ -30,17 +35,21 @@ export class CharacterRollerComponent {
 
   roll() {
     this.npc = this.npcGenerator.Generate();
+    this.characters = [...this.characters, this.npc];
   }
 
   GetGender(gender: Gender) {
     return Gender[gender];
   }
-  
-  GetGenders(): Gender[]{
+
+  GetGenders(): Gender[] {
     return <Gender[]>Object.values(Gender).filter(value => !isNaN(Number(value)));
   }
 
-  get IsRuler(){
+  SelectCharacter(character: NPC): void {
+    this.npc = character;
+  }
+  get IsRuler() {
     return this.npc?.Job.trim().toLowerCase() === "ruler";
   }
 }
