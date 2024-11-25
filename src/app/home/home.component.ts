75f6b615002a5566cb04afbd59af6d5f97a24806
Anthony C. Jones <randomgeekdom@gmail.com>
2024-11-25 10:01:41 -0600

    Making things prettier

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    MatCardActions,
+    MatCardContent,
+    MatCardTitle,
+    MatCardHeader,
+    MatCard,
+    MatButton
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/terra-machina/terra-machina.component.ts
+++ src/app/terra-machina/terra-machina.component.ts
@@ -1,17 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
-import { BrowserModule } from '@angular/platform-browser';
+import {MatButton} from "@angular/material/button";
 
 
 @Component({
   selector: 'app-terra-machina',
   standalone: true,
-  imports: [MatCardModule, CommonModule],
+  imports: [MatCardModule, CommonModule, MatButton],
   templateUrl: './terra-machina.component.html',
   styleUrl: './terra-machina.component.scss'
 })
 export class TerraMachinaComponent {
 
+  openUrl(pdf: string) {
+    const link = document.createElement('a');
+    link.href = pdf;
+    // @ts-ignore
+    link.setAttribute('download', pdf.split('/').pop());
+    link.style.display = 'none';
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+  }
 }
