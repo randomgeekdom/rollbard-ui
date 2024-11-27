095c9e324dba32a7c2ac8edcf0b1d965a2f8c21d
Anthony C. Jones <randomgeekdom@gmail.com>
2024-11-27 17:12:18 -0600

    Adding icons to rollers on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {MatButton} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,7 @@ import {MatButton} from "@angular/material/button";
     MatCardTitle,
     MatCardHeader,
     MatCard,
+    MatIconModule,
     MatButton
   ],
   templateUrl: './home.component.html',
