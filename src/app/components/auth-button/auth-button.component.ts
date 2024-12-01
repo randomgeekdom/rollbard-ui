9afbb5e509ef48ac5e3ddf54a3725a040c06f742
Anthony C. Jones <randomgeekdom@gmail.com>
2024-12-01 15:48:07 -0600

    oops forgot to include auth button

---
+++ src/app/components/auth-button/auth-button.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import {AsyncPipe, CommonModule, DOCUMENT, NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-auth-button',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    CommonModule
+  ],
+  templateUrl: './auth-button.component.html',
+  styleUrl: './auth-button.component.scss'
+})
+export class AuthButtonComponent {
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+
+}
