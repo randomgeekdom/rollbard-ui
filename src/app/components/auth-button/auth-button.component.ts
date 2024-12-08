ce2b1209342b04a4470c60a9e7914849515f48b9
Anthony C. Jones <randomgeekdom@gmail.com>
2024-12-08 16:18:16 -0600

    Fixing auth button display

--- src/app/components/auth-button/auth-button.component.ts
+++ src/app/components/auth-button/auth-button.component.ts
@@ -1,14 +1,16 @@
 import { Component, Inject } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import {AsyncPipe, CommonModule, DOCUMENT} from '@angular/common';
+import {MatListItem} from "@angular/material/list";
 
 @Component({
   selector: 'app-auth-button',
   standalone: true,
-  imports: [
-    AsyncPipe,
-    CommonModule
-  ],
+    imports: [
+        AsyncPipe,
+        CommonModule,
+        MatListItem
+    ],
   templateUrl: './auth-button.component.html',
   styleUrl: './auth-button.component.scss'
 })
