7555527dc2bb210bbbd84cc81f3fe0f455fa69a8
Anthony C. Jones <randomgeekdom@gmail.com>
2025-01-26 11:05:35 -0600

    Adding more to reviews

--- src/app/models/reviews/review.ts
+++ src/app/models/reviews/review.ts
@@ -1,7 +1,15 @@
-export default interface Review {
+export default class Review {
   date: Date;
   header: string;
   medium: string;
   rating: number;
   review: string;
+
+  constructor(date: Date, header: string, medium: string, rating: number, review: string) {
+    this.date = date;
+    this.header = header;
+    this.medium = medium;
+    this.rating = rating;
+    this.review = review;
+  }
 }
