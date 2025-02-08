b89fda5fc4107cd2d240aaf6a27c364636edd693
Anthony C. Jones <randomgeekdom@gmail.com>
2025-02-07 20:09:20 -0600

    Prettier authentication icons

--- src/app/components/auth-button/auth-button.component.ts
+++ src/app/components/auth-button/auth-button.component.ts
@@ -1,20 +1,23 @@
-import { Component, Inject } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular';
+import {Component, Inject} from '@angular/core';
+import {AuthService} from '@auth0/auth0-angular';
 import {AsyncPipe, CommonModule, DOCUMENT} from '@angular/common';
-import {MatListItem} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-auth-button',
   standalone: true,
-    imports: [
-        AsyncPipe,
-        CommonModule,
-        MatListItem
-    ],
+  imports: [
+    AsyncPipe,
+    CommonModule,
+    MatIcon,
+    MatButton
+  ],
   templateUrl: './auth-button.component.html',
   styleUrl: './auth-button.component.scss'
 })
 export class AuthButtonComponent {
-  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {
+  }
 
 }
