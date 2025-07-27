5c38a94899586006d8ff48a96378a1a80628d1bf
Anthony C. Jones <randomgeekdom@gmail.com>
2025-07-27 17:08:15 -0500

    Cleaning everything up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,8 @@ import {HomeComponent} from './home/home.component';
 import {TerraMachinaComponent} from './terra-machina/terra-machina.component';
 import {CharacterRollerComponent} from './character-roller/character-roller.component';
 import {FateCoreComponent} from './fate-core/fate-core.component';
-import {PlotRollerComponent} from './plot-roller/plot-roller.component';
 import {WorldRollerComponent} from "./world-roller/world-roller.component";
-import {ReviewsComponent} from "./reviews/reviews.component";
 import {FateCoreCharacterComponent} from "./fate-core/fate-core-character/fate-core-character.component";
-import {VigilantCityComponent} from "./vigilant-city/vigilant-city.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -22,9 +19,6 @@ export const routes: Routes = [
       {path: 'character', component: FateCoreCharacterComponent},
     ]
   },
-  {path: 'plot-roller', component: PlotRollerComponent, data: {title: 'Plot Roller', show: true}},
-  {path: 'reviews', component: ReviewsComponent, data: {title: 'Reviews', show: true}},
   {path: 'terra-machina', component: TerraMachinaComponent, data: {title: 'Terra Machina', show: true}},
-  {path: 'vigilant-city', component: VigilantCityComponent, data: {title: 'Vigilant City', show: true}},
   {path: 'world-roller', component: WorldRollerComponent, data: {title: 'World Roller', show: true}}
 ];
